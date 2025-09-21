e294c0b770fa78b3711515df5ca85647c29730b6
Mundiaa <mundiamanuel5@gmail.com>
2025-09-21 08:24:42 +0300

    Updated

--- conf.php
+++ conf.php
@@ -2,12 +2,12 @@
 // Site Information
 $conf['site_name'] = 'Notez Wiz';
 $conf['site_url'] = 'http://localhost';
-$conf['admin_email'] = ''; //your admin email
+$conf['admin_email'] = 'emmanuel.githaiga@strathmore.edu'; //your admin email
 
 // Database Configuration (MariaDB)
 $conf['db_host'] = 'localhost';
 $conf['db_user'] = 'root';
-$conf['db_pass'] = '';   //your MariaDB password
+$conf['db_pass'] = 'Admin123';   //your MariaDB password
 $conf['db_name'] = 'notez_wiz'; //your database name
 
 // Site Language
@@ -16,7 +16,7 @@ $conf['site_lang'] = 'en';
 // Email Configuration
 $conf['mail_type'] = 'smtp';
 $conf['smtp_host'] = 'smtp.gmail.com';
-$conf['smtp_user'] = ''; //your SMTP email
-$conf['smtp_pass'] = ''; //your SMTP email password
+$conf['smtp_user'] = 'mundiamanuel5@gmail.com'; //your SMTP email
+$conf['smtp_pass'] = 'ttrz tzax fozv afvq'; //your SMTP email password
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
--- database.php
+++ database.php
@@ -3,7 +3,7 @@ require_once 'conf.php';
 
 try {
     $dsn = "mysql:host={$conf['MariaDB']};dbname={$conf['notez_wiz']};charset=utf8mb4";
-    $pdo = new PDO($dsn, $conf['root'], $conf['<!--your MariaDB password-->']);
+    $pdo = new PDO($dsn, $conf['root'], $conf['Admin123']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "MariaDB connected successfully!";
 } catch (PDOException $e) {
