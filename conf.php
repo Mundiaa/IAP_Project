0a28becaa641b6bbc50fa340331f689e3bd9013d
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-10-22 13:49:28 +0300

    changes

--- conf.php
+++ conf.php
@@ -4,14 +4,14 @@ $conf = [
     
     // Site Information
     'site_name' => 'Notez Wiz',
-    'site_url' => 'http://localhost:8081/IAP_Project', //File location for the project
-    'admin_email' => 'emmanuel.githaiga@strathmore.edu', //your admin email
+    'site_url' => 'http://localhost/project/IAP_Project', //File location for the project
+    'admin_email' => 'emmanuel.wandibba@strathmore.edu', //your admin email
 
     // Database Configuration
-    'db_host' => '127.0.0.1',
-    'db_port' => 3307, //your MariaDB port
+    'db_host' => 'localhost',
+    'db_port' => 3306, //your MariaDB port
     'db_user' => 'root',
-    'db_pass' => 'admin123', //your MariaDB password
+    'db_pass' => '1234', //your MariaDB password
     'db_name' => 'notez_wiz', //your database name
 
     // Site Language
@@ -20,8 +20,8 @@ $conf = [
     // SMTP Configuration
     'mail_type' => 'smtp',
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_user' => 'mundiamanuel5@gmail.com', //your SMTP email
-    'smtp_pass' => 'ttrz tzax fozv afvq', // Gmail app password
+    'smtp_user' => 'emmanuel.wandibba@gmail.com', //your SMTP email
+    'smtp_pass' => 'eyol iqqe asob gkro', // Gmail app password
     'smtp_port' => 465,
     'smtp_secure' => 'ssl'
 ];
