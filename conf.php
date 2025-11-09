ed6f856e602b12e9a776b5189b7e6b9d3e67a9b5
Mundiaa <mundiamanuel5@gmail.com>
2025-11-09 16:16:54 +0300

    Updated

--- conf.php
+++ conf.php
@@ -9,20 +9,20 @@ $conf = [
     // Site Information
     'site_name' => 'Notez Wiz',
     'site_url' => 'http://localhost/project/IAP_Project', //File location for the project
-    'admin_email' => 'emmanuel.wandibba@strathmore.edu', //your admin email
+    'admin_email' => '', //your admin email
 
     // Database Configuration
     'db_host' => 'localhost',
     'db_port' => 3306, //your MariaDB port
     'db_user' => 'root',
-    'db_pass' => '1234', //your MariaDB password
+    'db_pass' => '', //your MariaDB password
     'db_name' => 'notez_wiz', //your database name
 
     // SMTP Configuration
     'mail_type' => 'smtp',
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_user' => 'emmanuel.wandibba@gmail.com', //your SMTP email
-    'smtp_pass' => 'eyol iqqe asob gkro', // Gmail app password
+    'smtp_user' => '', //your SMTP email
+    'smtp_pass' => '', // Gmail app password
     'smtp_port' => 465,
     'smtp_secure' => 'ssl'
 ];
